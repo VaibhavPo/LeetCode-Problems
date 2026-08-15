ee45fade4a8bcbd2217416184336e27ccaa9363e
Vaibhav Pokhriyal <111881524+VaibhavPo@users.noreply.github.com>
2026-08-15 23:34:00 +0530

    Time: 7 ms (0.97%), Space: 12.3 MB (90.71%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        nb = [-1]*(n+1)
+        
+        t = 0
+        def find(a,t):
+            if (a == 0):
+                t += nums[0]
+            elif  a==1:
+                t += max(nums[1], nums[0])
+            else:
+                x,y = nb[a-2], nb[a-1]
+                if x != -1:
+                   t += max(x + nums[a], find(a-1,t)) 
+                elif y != -1:
+                    t += max(find(a-2,t) + nums[a], y)
+                else:
+                    t += max(find(a-2,t) + nums[a], find(a-1,t))
+                nb[a] = t
+            return t
+
+        return find(n-1,t)
