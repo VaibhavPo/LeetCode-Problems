bf4a377953b5fbc50db99a8183e138a3b893be34
Vaibhav Pokhriyal <111881524+VaibhavPo@users.noreply.github.com>
2026-08-15 23:56:22 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,25 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if not nums:
+            return 0
+            
         n = len(nums)
-        nb = [-1]*(n+1)
+        # We only need an array of size n to store the states
+        nb = [-1] * n
         
-        t = 0
-        def find(a,t):
-            if (a == 0):
-                t += nums[0]
-            elif  a==1:
-                t += max(nums[1], nums[0])
-            else:
-                x,y = nb[a-2], nb[a-1]
-                if x != -1:
-                   t += max(x + nums[a], find(a-1,t)) 
-                elif y != -1:
-                    t += max(find(a-2,t) + nums[a], y)
-                else:
-                    t += max(find(a-2,t) + nums[a], find(a-1,t))
-                nb[a] = t
-            return t
+        def find(a):
+            # Base cases: return immediately
+            if a == 0:
+                return nums[0]
+            if a == 1:
+                return max(nums[1], nums[0])
+                
+            # Return memoized result if already calculated
+            if nb[a] != -1: 
+                return nb[a]
+                
+            # The core logic: Max of (rob current + rob a-2) OR (skip current and rob a-1)
+            nb[a] = max(find(a-2) + nums[a], find(a-1))
+            return nb[a]
 
-        return find(n-1,t)
+        return find(n - 1)
 
