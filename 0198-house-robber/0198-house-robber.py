224d9f83b1f4d9f831d6d492fb4e09c52f26cc7d
Vaibhav Pokhriyal <111881524+VaibhavPo@users.noreply.github.com>
2026-08-16 12:24:53 +0530

    Time: 3 ms (6.52%), Space: 12.6 MB (21.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -4,37 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # if not nums:
-        #     return 0
-            
-        # n = len(nums)
-        # # We only need an array of size n to store the states
-        # nb = [-1] * n
+        n = len(nums)
+        nb = [-1]*(n+1)
         
-        # def find(a):
-        #     # Base cases: return immediately
-        #     if a == 0:
-        #         return nums[0]
-        #     if a == 1:
-        #         return max(nums[1], nums[0])
-                
-        #     # Return memoized result if already calculated
-        #     if nb[a] != -1: 
-        #         return nb[a]
-                
-        #     # The core logic: Max of (rob current + rob a-2) OR (skip current and rob a-1)
-        #     nb[a] = max(find(a-2) + nums[a], find(a-1))
-        #     return nb[a]
+        t = 0
+        def find(a,t):
+            if (a == 0):
+                t += nums[0]
+            elif  a==1:
+                t += max(nums[1], nums[0])
+            else:
+                x,y = nb[a-2], nb[a-1]
+                if x != -1:
+                   t += max(x + nums[a], find(a-1,t)) 
+                elif y != -1:
+                    t += max(find(a-2,t) + nums[a], y)
+                else:
+                    t += max(find(a-2,t) + nums[a], find(a-1,t))
+                nb[a] = t
+            return t
 
-        # return find(n - 1)
+        return find(n-1,t)
 
-        n =len(nums)
-        if n <= 1:
-            return nums[0]
-        nb = [-1] *n
-        nb[0] = nums[0]
-        nb[1] = max(nums[0],nums[1])
-        for i in range(2, n):
-            find = max(nums[i] + nb[i-2], nb[i-1])
-            nb[i] = find
-        return nb[-1]
